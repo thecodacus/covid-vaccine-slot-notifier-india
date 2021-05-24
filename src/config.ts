7690e3f6a3d810478889354d12fc134f24dcec03
Anirban Kar <thecodacus@gmail.com>
2021-05-24 13:53:52 +0530

    basic template ready

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const config = {
+	getUrl: (pincode: number, date: Date) => {
+		let day = (date.getDate() + 100).toString().substring(1);
+		let month = (date.getMonth() + 100).toString().substring(1);
+		let year = (date.getFullYear() + 1000).toString().substring(2);
+		return `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${month}-${year}`;
+	},
+	header: { "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36" },
+};
